f8168d6f9ed6d3159c8608067aa7dbc698cc4758
Justin Hoch <justinhoch22@gmail.com>
2021-09-05 15:37:13 -0500

    Added About component

---
+++ src/components/aboutme/About.js
@@ -0,0 +1,16 @@
+import './about.css';
+
+function About() {
+    return(
+        <div className="aboutme">
+            <h2>About Me</h2>
+            <img src="./images/justin.PNG" alt="Me" width="150px" height="225px"></img>
+            <p> I am an 18 year old student who is learning to become a full stack web developer. 
+                I am currently working as a cashier at a gas station. I stream for fun when I am 
+                feeling up to it. I know python programming pretty well and am now also getting to 
+                know html and css as well.</p>
+        </div>
+    )
+}
+
+export default About;
