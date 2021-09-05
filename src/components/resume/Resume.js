ef6f451bc6c3056df2535db35ff2bbb88acfacec
Justin Hoch <justinhoch22@gmail.com>
2021-09-05 15:40:50 -0500

    Added Resume component

---
+++ src/components/resume/Resume.js
@@ -0,0 +1,26 @@
+import './resume.css';
+
+function Resume() {
+    let front = ["HTML", "CSS", "JavaScript", "JQuery", "Responsive Design", "React"]
+    let back = ["APIs", "Node", "Express", "MySQL", "Sequelize", "MongoDB", "Mongoose", "REST"]
+    return (
+        <div className="resume">
+            <h2>Resume</h2>
+            <br />
+            <br />
+            <p>You can download my resume <a target="_blank" href="./resume.pdf">here</a></p>
+            <br />
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                {front.map((item, i) => <li key={i}>{item}</li>)}
+            </ul>
+            <br />
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+                {back.map((item, i) => <li key={i}>{item}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default Resume;
