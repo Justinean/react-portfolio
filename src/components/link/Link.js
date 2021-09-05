24853debc1bf59a1bc2d50c8ea935be3e09b280b
Justin Hoch <justinhoch22@gmail.com>
2021-09-05 15:54:24 -0500

    Fixed portfolio linked

--- src/components/link/Link.js
+++ src/components/link/Link.js
@@ -2,7 +2,7 @@ import './link.css';
 
 function Links({currentPage, handlePageChange, locationName, displayName}) {
     return (
-            <a href={`/#${locationName}`} className={currentPage === locationName ? "active navlink hidden" : "navlink hidden"} onClick={() => handlePageChange(locationName)}>{displayName}</a>
+            <a href={`/react-portfolio/#${locationName}`} className={currentPage === locationName ? "active navlink hidden" : "navlink hidden"} onClick={() => handlePageChange(locationName)}>{displayName}</a>
     )
 }
 
