33423b556d3b37bd5387bc5e83c0c8a6b2764710
Justin Hoch <justinhoch22@gmail.com>
2021-09-05 15:39:22 -0500

    Added Footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import './footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="images">
+            <a href="https://github.com/Justinean" target="_blank" rel="noreferrer"><img src="./images/github.png" alt="Github cat" width="50px" height="50px" target="_blank"></img></a>
+            <a href="https://www.linkedin.com/in/justin-hoch-0b84651b6/" target="_blank" rel="noreferrer"><img src="./images/linkedin.png" alt="LinkedIn icon" width="50px" height="50px"></img></a>
+            <a href="https://www.instagram.com/SlateusTV/" target="_blank" rel="noreferrer"><img src="./images/instagram.png" alt="Instagram icon" width="50px" height="50px"></img></a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
