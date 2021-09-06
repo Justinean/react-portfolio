362bcaa927e78704cb0b66a79f34772c530a8d96
Justin Hoch <justinhoch22@gmail.com>
2021-09-06 18:25:46 -0500

    Updated my bio

--- src/components/aboutme/About.js
+++ src/components/aboutme/About.js
@@ -5,10 +5,11 @@ function About() {
         <div className="aboutme">
             <h2>About Me</h2>
             <img src="./images/justin.PNG" alt="Me" width="150px" height="225px"></img>
-            <p> I am an 18 year old student who is learning to become a full stack web developer. 
-                I am currently working as a cashier at a gas station. I stream for fun when I am 
-                feeling up to it. I know python programming pretty well and am now also getting to 
-                know html and css as well.</p>
+            <p> I am an 19 year old student who is learning to become a full stack web developer where 
+                I can create and maintain websites both on the front end, and back end. 
+                I am currently unemployed so I could focus more on my schooling. I stream and play games 
+                offline for fun. I have graduated from high school with honors at a 3.5 gpa. My skillset 
+                includes python, javascript, css, html, react, express and a couple other libraries</p>
         </div>
     )
 }
