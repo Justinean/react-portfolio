db75efc73cd4cb2a43d1ca0b43c1d1f067d3e362
Justin Hoch <justinhoch22@gmail.com>
2021-09-06 18:31:39 -0500

    Removed a comma

--- src/components/aboutme/About.js
+++ src/components/aboutme/About.js
@@ -6,7 +6,7 @@ function About() {
             <h2>About Me</h2>
             <img src="./images/justin.PNG" alt="Me" width="150px" height="225px"></img>
             <p> I am an 19 year old student who is learning to become a full stack web developer where 
-                I can create and maintain websites both on the front end, and back end. 
+                I can create and maintain websites both on the front end and back end. 
                 I am currently unemployed so I could focus more on my schooling. I stream and play games 
                 for fun. I have graduated from high school with honors at a 3.5 gpa. My skillset 
                 includes python, javascript, css, html, react, express and a couple other libraries. </p>
