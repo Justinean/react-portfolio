51538a38128d1ce13c0dcf0711b9c8c17e611bc6
Justin Hoch <justinhoch22@gmail.com>
2021-09-06 19:32:12 -0500

    Fixed text sizing issue

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -8,6 +8,7 @@ function Project({imageName, altTag, languages, name, link, repo}) {
                 <div className="layer" onClick={() => window.open(link, "_blank")}>
                     <div className="text" onClick={() => window.open(link, "_blank")}>
                         <h2>{name}</h2>
+                        <br />
                         <img src="./images/github.png" alt="GitHub Cat" width="50px" height="50px" onClick={() => window.open(`https://github.com/Justinean/${repo}`, "_blank")} />
                         <p>{languages}</p>
                     </div>
