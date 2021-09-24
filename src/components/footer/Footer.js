bc163617a76318be3652320332c86814cb1d95e4
Justin Hoch <justinhoch22@gmail.com>
2021-09-23 19:02:50 -0500

    Added mailto link to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
             <a href="https://github.com/Justinean" target="_blank" rel="noreferrer"><img src="./images/github.png" alt="Github cat" width="50px" height="50px" target="_blank"></img></a>
             <a href="https://www.linkedin.com/in/justin-hoch-0b84651b6/" target="_blank" rel="noreferrer"><img src="./images/linkedin.png" alt="LinkedIn icon" width="50px" height="50px"></img></a>
             <a href="https://www.instagram.com/SlateusTV/" target="_blank" rel="noreferrer"><img src="./images/instagram.png" alt="Instagram icon" width="50px" height="50px"></img></a>
+            <a href="mailto:justinonlycoding@gmail.com" rel="noreferrer"><img src="./images/gmail.png" alt="Gmail icon" width="50px" height="50px"></img></a>
             </div>
         </div>
     )
